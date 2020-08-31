d5b061251f6ee7e69a70012ae7873a7c65c4a61b
Vaclav Haisman <vhaisman@gmail.com>
2020-08-31 20:59:06 +0200

    Initial import.

---
+++ czech-typography-fixer/src/czech-typography-fixer.js
@@ -0,0 +1,165 @@
+console.log('Czech typography fixer here.');
+// window.addEventListener ("load", czech_typography_fixer_main, true);
+
+function czechTypographyFixerMain () {
+    var nodes = [];
+
+    var host = document.location.host;
+    console.log('location.host: ' + host);
+    if (host.startsWith('www.ceska-justice.cz')
+      || host.startsWith('www.zdravotnickydenik.cz')) {
+        console.log('using rules for Česká Justice');
+        nodes = document.body.querySelectorAll('article.post');
+    } else if (/^(.+\.)?ihned\.(cz|news)$/.test(host)) {
+        console.log('using rules for IHned');
+        nodes = document.body.querySelectorAll('article, .perex, div[id=main-content], div[id=content] h1, div.content-column, div.article');
+    } else if (host.endsWith('.aktualne.cz')) {
+        console.log('using rules for Aktualne');
+        nodes = document.body.querySelectorAll('div.clanek, div.titulek-clanku, div.obsah, div.page');
+    } else if (host === 'www.ceskenoviny.cz') {
+        console.log('using rules for České Noviny');
+        nodes = document.body.querySelectorAll('div.box-article');
+    } else if (host.endsWith('.idnes.cz')) {
+        console.log('using rules for IDnes');
+        nodes = document.body.querySelectorAll('div.art-full, div.col, div.hp-b, div[id=main] div[id=content]');
+    } else if (host.endsWith('.reflex.cz')) {
+        console.log('using rules for Reflex');
+        nodes = document.body.querySelectorAll('article');
+    } else if (host.endsWith('.respekt.cz')) {
+        console.log('using rules for Respekt');
+        nodes = document.body.querySelectorAll('div.layout_content-text, div.layout_content-full');
+    } else if (host.endsWith('.ceskatelevize.cz')) {
+        console.log('using rules for Česká televize');
+        nodes = document.body.querySelectorAll('article, div[id=myPage] div[id=mainPanel]');
+    } else if (host.endsWith('.seznam.cz') || host.endsWith('.seznamzpravy.cz')) {
+        console.log('using rules for Seznam');
+        nodes = document.body.querySelectorAll('main');
+    } else if (/^(.+\.)?neviditelnypes.lidovky\.cz$/.test(host)) {
+        console.log('using rules for Neviditelný Pes');
+        nodes = document.body.querySelectorAll('div[id=art-full], div[id=col-c]');
+    } else if (host.endsWith('.lidovky.cz')) {
+        console.log('using rules for Lidovky');
+        nodes = document.body.querySelectorAll('div[id=content]');
+    } else if (host === 'svobodneforum.cz') {
+        console.log('using rules for Svobodné fórum');
+        nodes = document.body.querySelectorAll('main');
+    } else if (host.endsWith('.rozhlas.cz')) {
+        console.log('using rules for Rozhlas');
+        nodes = document.body.querySelectorAll('div.main, div[id=main], div[id=aside]');
+    } else if (host === 'mediahub.cz') {
+        console.log('using rules for Mediahub');
+        nodes = document.body.querySelectorAll('main section article, section[id=section-content], section[id=section-sidebar], div.container');
+    } else if (host.endsWith('.novinky.cz')) {
+        console.log('using rules for Novinky');
+        nodes = document.body.querySelectorAll('main[role=main], div[id=articleBox], div[id=articleContent], div[id=articleHeaderBig], div[id=page] div[id=subpage] div[id=main]');
+    } else if (host.startsWith('news.google.')) {
+        console.log('using rules for Google News');
+        nodes = document.body.querySelectorAll('div.section-stream-content');
+    } else if (host.endsWith('.info.cz')) {
+        console.log('using rules for Info');
+        nodes = document.body.querySelectorAll('article');
+    } else if (host.endsWith('.tyden.cz')) {
+        console.log('using rules for Týden');
+        nodes = document.body.querySelectorAll('div.article, div.articles');
+    } else if (host.endsWith('cs.wikipedia.org')) {
+        console.log('using rules for Česká Wikipedie');
+        nodes = document.body.querySelectorAll('div[id=bodyContent]');
+    } else if (host.endsWith('echo24.cz')) {
+        console.log('using rules for Echo24');
+        nodes = document.body.querySelectorAll('div[id=content], article, section');
+    } else if (host.endsWith('parlamentnilisty.cz')) {
+        console.log('using rules for Parlamentni Listy');
+        nodes = document.body.querySelectorAll('div[id=main]');
+    } else if (/^(.+\.)?forum24\.cz$/.test(host)) {
+        console.log('using rules for Forum24');
+        nodes = document.body.querySelectorAll('article');
+    } else if (/^(.+\.)?euro\.cz$/.test(host)) {
+        console.log('using rules for Euro.cz');
+        nodes = document.body.querySelectorAll('div[id=article]');
+    } else if (/^(.+\.)?zpovednice\.cz$/.test(host)) {
+        console.log('using rules for Zpovednice');
+        nodes = document.body.querySelectorAll('td.conftext, td.absoltext');
+    } else if (/^(.+\.)?e15\.cz$/.test(host)) {
+        console.log('using rules for E15');
+        nodes = document.body.querySelectorAll('article');
+    } else if (/^(.+\.)?neovlivni\.cz$/.test(host)) {
+        console.log('using rules for Neovlivni');
+        nodes = document.body.querySelectorAll('section');
+    } else if (/^www\.sysifos\.cz$/.test(host)) {
+        console.log('using rules for sisyfos.cz');
+        nodes = document.body.querySelectorAll('div[id=blok005a]');
+    } else if (/^(.+\.)?denikreferendum\.cz$/.test(host)) {
+        console.log('using rules for Deník Referendum');
+        nodes = document.body.querySelectorAll('div[id=content]');
+    } else if (/^(.+\.)?denik\.cz$/.test(host)) {
+        console.log('using rules for Deník');
+        nodes = document.body.querySelectorAll('div.content');
+    } else if (/^(.+\.)?irozhlas\.cz$/.test(host)) {
+        console.log('using rules for iRozhlas');
+        nodes = document.body.querySelectorAll('article');
+    } else if (/^(.+\.)?epravo\.cz$/.test(host)) {
+        console.log('using rules for epravo.cz');
+        nodes = document.body.querySelectorAll('.article, .article-detail');
+    } else if (/^www\.ahaonline\.cz$/.test(host)) {
+        console.log('using rules for ahaonline.cz');
+        nodes = document.body.querySelectorAll('article');
+    } else {
+        console.log('No rule matched.');
+    }
+    console.log('Got ' + nodes.length + ' nodes.');
+
+    var textNodes = [];
+    var i;
+    for (i = 0; i !== nodes.length; ++i) {
+        var node = nodes[i];
+        var walk = document.createTreeWalker(node, NodeFilter.SHOW_TEXT,
+            {
+                acceptNode: function (n) {
+                    if (n.parentNode && n.parentNode.nodeName === 'SCRIPT') {
+                    // console.log('rejecting text of node ' + n.parentNode.nodeName);
+                        return NodeFilter.FILTER_REJECT;
+                    } else {
+                    // console.log('accepting text of node ' + n.parentNode.nodeName);
+                        return NodeFilter.FILTER_ACCEPT;
+                    }
+                }
+            }, false);
+
+        var textNode;
+        while ((textNode = walk.nextNode())) {
+            textNodes.push(textNode);
+        }
+    }
+
+    for (i = 0; i !== textNodes.length; ++i) {
+        transmogrifyTextNode(textNodes[i]);
+    }
+}
+
+const czechTypographyFixerReSingleLetters = /(\b[zZsSuUkKoOvViI])[ ]+/gu;
+const czechTypographyFixerReSingleA = /(\b[aA])[ ]+/gu;
+const czechTypographyFixerRePercents = /([0-9])[ ]([%‰°℃℉])/gu;
+const czechTypographyFixerReDates = /(\d{1,2}\.)[ ](ledna|února|března|dubna|května|června|července|srpna|září|října|listopadu|prosince)/gu;
+const czechTypographyFixerReStraightToCzechQuotes = /([^0-9]|^)"([^"]*)"/gu;
+// const czechTypographyFixerReEnglishToCzechQuotes = /“([^”]*)”/gu;
+const czechTypographyFixerReSingleQuotes = /([^0-9a-z]|^)'([^']*)'/igu;
+const czechTypographyFixerReFixBoth99Quotes = /„([^”]*)”/gu;
+
+function transmogrifyTextNode (textNode) {
+    var text = textNode.nodeValue;
+
+    text = text.replace(czechTypographyFixerReSingleLetters, '$1\u00a0');
+    text = text.replace(czechTypographyFixerReSingleA, '$1\u00a0');
+    text = text.replace(czechTypographyFixerRePercents, '$1\u00a0$2');
+    text = text.replace(czechTypographyFixerReDates, '$1\u00a0$2');
+    // XXX This breaks proper Czech quotes if they are already in the text multiple times.
+    // text = text.replace(czechTypographyFixerReEnglishToCzechQuotes, '„$1“');
+    text = text.replace(czechTypographyFixerReStraightToCzechQuotes, '$1„$2“');
+    text = text.replace(czechTypographyFixerReFixBoth99Quotes, '„$1“');
+    text = text.replace(czechTypographyFixerReSingleQuotes, '$1‚$2‘');
+
+    // console.log("text after: >" + text + "<");
+    textNode.nodeValue = text;
+}
+
+czechTypographyFixerMain();
---
+++ czech-typography-fixer/webpack.config.js
@@ -0,0 +1,109 @@
+const path = require('path');
+const WebpackUserscript = require('webpack-userscript');
+const JsDocPlugin = require('jsdoc-webpack-plugin');
+
+var includeOnSites = [
+    /^https?:\/\/www\.ceska-justice\.cz\/.*$/,
+    /^https?:\/\/www\.zdravotnickydenik\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?ihned\.(cz|news)\/.*$/,
+    /^https?:\/\/.+\.aktualne\.cz\/.*$/,
+    /^https?:\/\/www\.ceskenoviny\.cz\/.*$/,
+    /^https?:\/\/.+\.idnes\.cz\/.*$/,
+    /^https?:\/\/.+\.reflex\.cz\/.*$/,
+    /^https?:\/\/.+\.respekt\.cz\/.*$/,
+    /^https?:\/\/.+\.ceskatelevize\.cz\/.*$/,
+    /^https?:\/\/.+\.seznam\.cz\/.*$/,
+    /^https?:\/\/.+\.seznamzpravy\.cz\/.*$/,
+    /^https?:\/\/.+\.lidovky\.cz\/.*$/,
+    /^https?:\/\/svobodneforum\.cz\/.*$/,
+    /^https?:\/\/.+\.rozhlas\.cz\/.*$/,
+    /^https?:\/\/mediahub\.cz\/.*$/,
+    /^https?:\/\/.+\.novinky\.cz\/.*$/,
+    /^https?:\/\/news\.google\.com\/.*$/,
+    /^https?:\/\/.+\.info\.cz\/.*$/,
+    /^https?:\/\/.+\.tyden\.cz\/.*$/,
+    /^https?:\/\/cs\.wikipedia\.org\/.*$/,
+    /^https?:\/\/echo24\.cz\/.*$/,
+    /^https?:\/\/.+\.parlamentnilisty\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?forum24\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?euro\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?zpovednice\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?e15\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?neovlivni\.cz\/.*$/,
+    /^https?:\/\/www\.sysifos\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?denikreferendum\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?neviditelnypes.lidovky\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?denik\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?irozhlas\.cz\/.*$/,
+    /^https?:\/\/(.+\.)?epravo\.cz\/.*$/,
+    /^https?:\/\/www\.ahaonline\.cz\/.*$/
+];
+includeOnSites = includeOnSites.map((x) => x.toString());
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        'czech-typography-fixer': path.join(__dirname, 'src', 'czech-typography-fixer.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'czech-typography-fixer.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    debug: true,
+                                    useBuiltIns: 'usage',
+                                    corejs: '3'
+                                }
+                            ]
+                        ],
+                        plugins: [
+                            'babel-plugin-minify-dead-code-elimination'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map',
+    optimization: {
+        minimize: false
+    },
+    plugins: [
+        new WebpackUserscript({
+            headers: {
+                name: '[name]',
+                version: '[version]',
+                author: 'wilx',
+                homepage: 'https://github.com/wilx/user-scripts/czech-typography-fixer',
+                namespace: 'https://github.com/wilx/user-scripts/czech-typography-fixer',
+                'run-at': 'document-end',
+                grant: 'none',
+                noframes: true,
+                include: includeOnSites
+            },
+            pretty: true
+        }),
+        new JsDocPlugin({
+            conf: path.join(__dirname, 'jsdoc.conf.json')
+        })
+    ],
+    resolve: {
+        alias: {
+            node_modules: path.join(__dirname, 'node_modules')
+        }
+    }
+};
