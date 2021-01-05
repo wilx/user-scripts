85265c6a44df4ba5f7467e0ed013af81dc31db38
Vaclav Haisman <vhaisman@gmail.com>
2021-01-06 00:05:39 +0100

    Updates.

--- hyphenator-for-czech-language/output/hyphenator-for-czech-language.user.js
+++ hyphenator-for-czech-language/output/hyphenator-for-czech-language.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        hyphenator-for-czech-language
-// @version     1.0.68
+// @version     1.0.72
 // @author      wilx
 // @description Hyphenator for news sitez in Czech
 // @homepage    https://github.com/wilx/user-scripts/hyphenator-for-czech-language
