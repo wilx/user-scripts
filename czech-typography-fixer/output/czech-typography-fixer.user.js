7ffc6c19c480330d4ed92b65a8c8ca5e96720f9a
Vaclav Haisman <vhaisman@gmail.com>
2022-12-23 20:07:15 +0100

    Recognize hn.cz as IHned.

--- czech-typography-fixer/output/czech-typography-fixer.user.js
+++ czech-typography-fixer/output/czech-typography-fixer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        czech-typography-fixer
-// @version     1.0.139
+// @version     1.0.141
 // @author      wilx
 // @description Czech typography fixer for Czech news sites.
 // @homepage    https://github.com/wilx/user-scripts/czech-typography-fixer
